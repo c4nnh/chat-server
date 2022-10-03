8d2361ac48a07637897bf7e1b451f9ca190ed844
c4nnh <cannh.99@gmail.com>
2022-10-03 14:28:28 +0700

    fix config

--- src/gateway/websocket.gateway.ts
+++ src/gateway/websocket.gateway.ts
@@ -18,11 +18,7 @@ import { PrismaService } from '../db/prisma.service'
 import { RelationUserEntity } from '../users/entities/relation-user.entity'
 import { RoomRole } from '@prisma/client'
 
-@WebSocketGateway({
-  cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-  },
-})
+@WebSocketGateway()
 export class MessagingGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
