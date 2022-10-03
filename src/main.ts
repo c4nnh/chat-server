51017bc58715732fd078383cc6d609666a839f55
c4nnh <cannh.99@gmail.com>
2022-10-03 18:41:29 +0700

    fix port

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('api', app, document)
 
-  await app.listen(port)
+  await app.listen(port, '0.0.0.0')
   console.log(`Server is running on port ${port}`)
 }
 bootstrap()
