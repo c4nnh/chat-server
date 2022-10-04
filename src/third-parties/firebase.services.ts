d5ce3abfe2a4d0d55935172fe375d4b8a9fa0e0f
c4nnh <cannh.99@gmail.com>
2022-10-04 17:34:41 +0700

    fix import

--- src/third-parties/firebase.services.ts
+++ src/third-parties/firebase.services.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { initializeApp, credential, app } from 'firebase-admin'
+import admin from 'firebase-admin'
 
 @Injectable()
 export class FirebaseService {
-  private app: app.App
+  private app: admin.app.App
 
   constructor(private readonly configService: ConfigService) {
-    this.app = initializeApp({
-      credential: credential.cert({
+    this.app = admin.initializeApp({
+      credential: admin.credential.cert({
         projectId: this.configService.get('FIREBASE_PROJECT_ID'),
         privateKey: this.configService.get('FIREBASE_PRIVATE_KEY'),
         clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
