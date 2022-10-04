39ce58eed05225ee7c779e828c9e1d9631879f0d
c4nnh <cannh.99@gmail.com>
2022-10-05 00:01:20 +0700

    fix fb

--- src/third-parties/firebase.services.ts
+++ src/third-parties/firebase.services.ts
@@ -8,39 +8,36 @@ export class FirebaseService {
 
   constructor(private readonly configService: ConfigService) {
     const bucketName = this.configService.get('FIREBASE_BUCKET_NAME')
-    // this.app = admin.initializeApp({
-    //   credential: admin.credential.cert({
-    //     projectId: this.configService.get('FIREBASE_PROJECT_ID'),
-    //     privateKey: this.configService
-    //       .get('FIREBASE_PRIVATE_KEY')
-    //       .replace(/\\n/g, '\n'),
-    //     clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
-    //   }),
-    //   storageBucket: bucketName,
-    // })
-    // this.app
-    //   .storage()
-    //   .bucket(bucketName)
-    //   .setCorsConfiguration([
-    //     {
-    //       origin: ['*'],
-    //     },
-    //   ])
+    this.app = admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: this.configService.get('FIREBASE_PROJECT_ID'),
+        privateKey: this.configService.get('FIREBASE_PRIVATE_KEY'),
+        clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
+      }),
+      storageBucket: bucketName,
+    })
+    this.app
+      .storage()
+      .bucket(bucketName)
+      .setCorsConfiguration([
+        {
+          origin: ['*'],
+        },
+      ])
 
     // if (!bukcet) {
     // }
 
-    //   this.app
-    //     .storage()
-    //     .bucket(bucketName)
-    //     .file('abcd')
-    //     .getSignedUrl({
-    //       version: 'v4',
-    //       action: 'write',
-    //       expires: new Date().getTime() + 1000 * 60 * 60, // 2 minutes
-    //       contentType: 'image/jpeg',
-    //     })
-    //     .then(value => console.log(value))
-    // }
+    this.app
+      .storage()
+      .bucket(bucketName)
+      .file('abcd')
+      .getSignedUrl({
+        version: 'v4',
+        action: 'write',
+        expires: new Date().getTime() + 1000 * 60 * 60, // 2 minutes
+        contentType: 'image/jpeg',
+      })
+      .then(value => console.log(value))
   }
 }
