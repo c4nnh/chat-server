1b71d5c9af7178fbaf1987969b6c2c18394e99b1
c4nnh <cannh.99@gmail.com>
2022-10-04 21:40:23 +0700

    add firebase service

--- src/third-parties/firebase.services.ts
+++ src/third-parties/firebase.services.ts
@@ -7,12 +7,37 @@ export class FirebaseService {
   private app: admin.app.App
 
   constructor(private readonly configService: ConfigService) {
+    const bucketName = this.configService.get('FIREBASE_BUCKET_NAME')
     this.app = admin.initializeApp({
       credential: admin.credential.cert({
         projectId: this.configService.get('FIREBASE_PROJECT_ID'),
         privateKey: this.configService.get('FIREBASE_PRIVATE_KEY'),
         clientEmail: this.configService.get('FIREBASE_CLIENT_EMAIL'),
       }),
+      storageBucket: bucketName,
     })
+    // this.app
+    //   .storage()
+    //   .bucket(bucketName)
+    //   .setCorsConfiguration([
+    //     {
+    //       origin: ['*'],
+    //     },
+    //   ])
+
+    // if (!bukcet) {
+    // }
+
+    this.app
+      .storage()
+      .bucket(bucketName)
+      .file('abcd')
+      .getSignedUrl({
+        version: 'v4',
+        action: 'write',
+        expires: new Date().getTime() + 1000 * 60 * 60, // 2 minutes
+        contentType: 'image/jpeg',
+      })
+      .then(value => console.log(value))
   }
 }
